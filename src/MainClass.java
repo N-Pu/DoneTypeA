2596086e4187cac9e9d2ab8e5d78cb94090b58ac
Harry.G <garry.garyra@mail.ru>
2021-07-11 12:04:20 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+        String input = "fdsfds342sdfds";
+        SubClass subClass = new SubClass();
+        for (int i = 0; i < input.length(); i++) {
+
+        }
+
+    }
+}
---
+++ src/SubClass.java
@@ -0,0 +1,17 @@
+public class SubClass {
+    private String str;
+    private String[] array;
+
+    public CharSequence getStrAndNum (CharSequence input){
+
+
+
+        return input;
+    }
+
+
+    public void inputAdder() {
+
+    }
+
+}
